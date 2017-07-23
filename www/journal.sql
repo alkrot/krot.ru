07a2059488a17aa454dc48ac0b07310cd79079e0
sdigi <graber.qwerty@mail.ru>
2017-07-23 22:18:10 +0300

    add bd structure

--- www/journal.sql
+++ www/journal.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Июн 24 2017 г., 21:59
+-- Время создания: Июл 23 2017 г., 23:14
 -- Версия сервера: 5.5.25
 -- Версия PHP: 5.3.13
 
@@ -31,8 +31,9 @@ CREATE TABLE IF NOT EXISTS `attachment` (
   `name` varchar(32) CHARACTER SET cp1251 NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=19 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
 --
 -- Структура таблицы `group`
@@ -43,8 +44,7 @@ CREATE TABLE IF NOT EXISTS `group` (
   `name` varchar(22) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
-
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -56,7 +56,7 @@ CREATE TABLE IF NOT EXISTS `list` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `series` varchar(48) NOT NULL COMMENT 'Индификатор',
   `attachment_id` int(11) NOT NULL COMMENT 'Принадлежность',
-  `number_al` int(11) NOT NULL COMMENT 'Номер Абонентской Линии',
+  `address` varchar(64) NOT NULL COMMENT 'Номер Абонентской Линии',
   `equipment` varchar(32) NOT NULL COMMENT 'Оборудование',
   `type_equipment` varchar(32) NOT NULL COMMENT 'Тип оборудования',
   `seal` tinyint(1) NOT NULL COMMENT 'Гарантия',
@@ -72,8 +72,9 @@ CREATE TABLE IF NOT EXISTS `list` (
   `contact` text NOT NULL COMMENT 'Контактное лицо',
   PRIMARY KEY (`id`),
   KEY `group_id` (`group_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+-- --------------------------------------------------------
 
 --
 -- Структура таблицы `users`
@@ -89,19 +90,14 @@ CREATE TABLE IF NOT EXISTS `users` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `login` (`login`),
   KEY `group_id` (`group_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=18 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
 
 --
 -- Дамп данных таблицы `users`
 --
 
 INSERT INTO `users` (`id`, `login`, `password`, `group_id`, `role`, `access_group`) VALUES
-(1, 'admin', '39ec37cd2ef52f5cf5e9449345f10dc737d5773c', 1, 4, ''),
-(9, 'bag007@list.ru', '961afb736cbe960fa3663ed785ca57ab4ceac675', 1, 1, ''),
-(14, 'nach', '0807af5c7ebb4961d037edfe1bd251687a9e6c66', 1, 1, '["\\u0413\\u0440\\u041e\\u041e","\\u0413\\u0440\\u0421\\u041e"]'),
-(15, 'secondLevel', '7b1bd877ed8d04f84700a45b7e024311c38be47a', 3, 2, '[""]'),
-(16, 'thirdLevel', '16e4a932b24bc284216f9ed3c6440b03fae07f0e', 2, 3, '[""]'),
-(17, 'test@mail.ru', '0d8848ed9760458ee797d0afe3222768834959b9', 3, 2, '[""]');
+(1, 'admin', '39ec37cd2ef52f5cf5e9449345f10dc737d5773c', 1, 4, '');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
